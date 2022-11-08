6f4a72324653dc2e15833694e47147417d267a7f
Akilandeshwari-Ravishankar <akilandeshwari.ravishankar@gmail.com>
2022-11-08 21:18:44 +0530

    modified routes

--- fse1-fidzulu/src/app/app-routing.module.ts
+++ fse1-fidzulu/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainPageComponent } from './main-page/main-page.component';
 import { BikesDisplayComponent } from './productDisplay/bikes-display/bikes-display.component';
 
-const routes: Routes = [{path:"bikes", component: BikesDisplayComponent},];
+const routes: Routes = [
+  {path:"", redirectTo: "/home", pathMatch: "full"},
+  {path:"home", component: MainPageComponent},
+  {path:"bikes", component: BikesDisplayComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- fse1-fidzulu/src/app/app.module.ts
+++ fse1-fidzulu/src/app/app.module.ts
@@ -14,6 +14,7 @@ import {MatCardModule} from '@angular/material/card';
 import { MatPaginatorModule} from '@angular/material/paginator';
 import {MatTooltipModule} from '@angular/material/tooltip';
 import { BikesDisplayComponent } from './productDisplay/bikes-display/bikes-display.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { BikesDisplayComponent } from './productDisplay/bikes-display/bikes-disp
     ToyTableComponent,
     FoodTableComponent,
     BikesTableComponent,
-    BikesDisplayComponent
+    BikesDisplayComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
